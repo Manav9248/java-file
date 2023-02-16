1775d6e0c7d72ece0085547100ad2225f484bbb7
Manav <117278007+Manav9248@users.noreply.github.com>
2023-02-16 20:45:03 +0530

    newFile

--- newFile.java
+++ newFile.java
@@ -1,25 +1,6 @@
-import java.util.Scanner;
-// program for Fibonaci series
-public class newFile {
-
+ import java.util.*;;
+ public class newFile {
     public static void main(String args[]) {
-       Scanner sc = new Scanner(System.in);
-       int n = sc.nextInt();
-
-       int a =0, b= 1;
-
-       System.out.println(a+" ");
-    
-     if(n>1) {
-        // find nth term
-        for(int i=2; i<=n; i++) {
-            System.out.println(b+" ");
-            // the concept below is called swaping
-            int temp =b;
-            b = a +b;
-            a= temp;
-        }
-        System.out.println();
-     }
-    }
-}
+  
+ }
+ }
---
+++ stringFile.java
@@ -0,0 +1,6 @@
+import java.util.*;;
+public class stringFile {
+    public static void main(String args[]) {
+       
+    }
+}
