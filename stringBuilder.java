e8e6ef1da65134f5881d11ced6677762516ce15f
Manav <117278007+Manav9248@users.noreply.github.com>
2023-02-17 12:50:40 +0530

    save

--- stringBuilder.java
+++ stringBuilder.java
@@ -1,5 +1,9 @@
 public class stringBuilder {
     public static void main(String args[]) {
-      
+      StringBuilder sb = new StringBuilder("HelloWorld");
+      System.out.println("String = "+sb);
+
+      //reversing of stringBuilder
+      System.out.println("reverse = "+sb.reverse());
     }
 }
