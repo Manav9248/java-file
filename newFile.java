43757482151265b354e75dc0d0f66f752306193c
Manav <117278007+Manav9248@users.noreply.github.com>
2023-02-20 20:06:27 +0530

    ok

--- newFile.java
+++ newFile.java
@@ -1,6 +1,9 @@
  import java.util.*;;
  public class newFile {
     public static void main(String args[]) {
- 
+ StringBuilder sb = new StringBuilder("hello");
+
+ sb.append(" stark");
+  System.out.println(sb.length());
  }
  }
