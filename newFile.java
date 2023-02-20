d8bfe9df920f90aeea5fe693ef8c49c8c90b1cef
Manav <117278007+Manav9248@users.noreply.github.com>
2023-02-20 20:40:59 +0530

    okk

--- newFile.java
+++ newFile.java
@@ -1,4 +1,4 @@
- import java.util.*;;
+ import java.util.*;
  public class newFile {
     public static void main(String args[]) {
  StringBuilder sb = new StringBuilder("hello");
---
+++ randomFile.java
@@ -0,0 +1,9 @@
+public class randomFile {
+    public static void main(String args[]) {
+        StringBuilder name = new StringBuilder("Manav");
+
+     name.append(" pal");
+     name.reverse();
+     System.out.println(name);
+    }
+}
