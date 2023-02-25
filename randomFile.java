8c591764d9cd64e0dcad389260e46ae00c1ed991
Manav <117278007+Manav9248@users.noreply.github.com>
2023-02-25 21:49:09 +0530

    ok

--- randomFile.java
+++ randomFile.java
@@ -1,9 +1,14 @@
 public class randomFile {
     public static void main(String args[]) {
-        StringBuilder name = new StringBuilder("Manav");
+   
+       int n = 5;  // 0101(in decimal)
+      int position = 3;
 
-     name.append(" pal");
-     name.reverse();
-     System.out.println(name);
+      int bitMask = 1<<position;
+
+      if((bitMask & n)==0);
+      System.out.println("bitmask is zero ");
+    } else {
+        System.out.println("Bitmask is not zero ");
     }
 }
